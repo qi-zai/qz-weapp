d396d683071bf458c6ab1b53e3445a3bc35ace16
zhangqi <15248448516@163.com>
2022-03-14 08:14:11 +0800

    build: init

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const { task, parallel, watch } = require('gulp')
 
 const init = require('./lib')
 
-module.exports = function () {
-  const options = {
+module.exports = function (options) {
+  const handlers = init({
     output: "dist",
-  }
+    ...options,
+    componentsMappers: {},
+  })
 
-  task('build', parallel(init(options)))
+  task('build', parallel(
+    handlers(['clean', 'vue2tpl', 'vue2css', 'vue2js', 'cpfile'])
+  ))
 
-  task('dev',() => watch('src/**/*', parallel(init(options))))
+  task('dev', function () {
+    watch('src/**/*', parallel(
+      handlers(['vue2tpl', 'vue2css', 'vue2js', 'cpfile'])
+    )).on('change', function (event) {
+      console.log(event)
+    })
+  })
 }
